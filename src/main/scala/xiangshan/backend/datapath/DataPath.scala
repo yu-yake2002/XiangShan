1b8dfedd46c117a2ca089fb386059165e2ce105e
yu-yake2002 <yuyake02@outlook.com>
2025-05-06 15:06:28 +0800

    fix(difftest): Fix connection between difftest wires and mtilex
    - Use mtilex values from rob for difftest.

--- src/main/scala/xiangshan/backend/datapath/DataPath.scala
+++ src/main/scala/xiangshan/backend/datapath/DataPath.scala
@@ -348,7 +348,6 @@ class DataPathImp(override val wrapper: DataPath)(implicit p: Parameters, params
   mxDiffReadData.foreach(_ := mxDiffRead
     .get._2
     .slice(0, 3)
-    .map(_(2, 0))
   )
 
   io.diffVl.foreach(_ := vlDiffReadData.get)
